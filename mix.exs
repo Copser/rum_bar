869f06dec135760f0ecc3da1e32e860cc064cc9a
Copser <ppilipovic84@gmail.com>
2019-06-26 16:22:46 +0200

    master
    - prep app for heroku

--- mix.exs
+++ mix.exs
@@ -64,6 +64,7 @@ defmodule RumBar.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
+      "heroku.reset": ["ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate", "test"]
